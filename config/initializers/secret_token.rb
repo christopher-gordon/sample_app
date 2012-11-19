c82c8eec831b77be2af0c497ef5840f57d7e8c4b
Chris Gordon <ChristopherScottGordon@gmail.com>
2012-11-19 12:18:16 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Concrit::Application.config.secret_token = 'be5918f0dbad0c6c1ec4f75a8405de196909a207aad2f12fed346337f945c6d0a3136cf82645516e573d567327dc5c29cd1d8f90d1cb21a171a53d4c70e88616'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Concrit::Application.config.session_store :cookie_store, key: '_concrit_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Concrit::Application.config.session_store :active_record_store
