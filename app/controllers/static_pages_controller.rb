65e1ebd8d98906e0a6489e01bc09972c311e0e69
Chris Gordon <ChristopherScottGordon@gmail.com>
2012-11-19 21:15:35 -0600

    Finish static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    @title = "Sample App | Home"
   end
 
   def help
+    @title = "Help"
+  end
+
+  def about
+    @title = "About Us"
   end
 end
